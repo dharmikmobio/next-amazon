3a8751d7a1ba12ba21972226ecfc5c838662666e
Dharmik <dharmikp.mobio@gmail.com>
2022-05-04 13:31:40 +0530

    Login Signup Func

---
+++ pages/shipping.js
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { useRouter } from 'next/router';
+import { Store } from '../utils/Store';
+
+export default function Shipping() {
+  const router = useRouter();
+  // eslint-disable-next-line no-unused-vars
+  const { state, dispatch } = useContext(Store);
+  const { userInfo } = state;
+  if (!userInfo) {
+    router.push('/login?redirect=/shipping');
+  }
+
+  return <div>Shipping page</div>;
+}
